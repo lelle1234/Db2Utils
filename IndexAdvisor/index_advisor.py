94ce839a4db8ee55644e625af2f11c1c4c8e5920
Lennart Jonsson <erik.lennart.jonsson@gmail.com>
2025-02-16 15:08:55 +0100

    Some tidying

--- IndexAdvisor/index_advisor.py
+++ IndexAdvisor/index_advisor.py
@@ -9,7 +9,11 @@ from functools import reduce
 from tabulate import tabulate
 # pip3 install ibm_db --user
 import ibm_db
-
+import logging
+import argparse
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 # init
 db = user = pwd = infile = None
@@ -101,137 +105,149 @@ order by length(indexes)-length(replace(indexes,',','')), total_cost desc"""
 DISPLAY_INDEXES = "select distinct cast(creation_text as varchar(2000)) from advise_index where name = ?"
 
 ORIGINAL_COST = "select total_cost from costtbl where indexes = ''"
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Index Evaluator')
+    parser.add_argument('-d', '--db', required=True, help='Database name')
+    parser.add_argument('-u', '--user', required=True, help='User name')
+    parser.add_argument('-p', '--pwd', required=True, help='Password')
+    parser.add_argument('-i', '--infile', required=True, help='Input file')
+    parser.add_argument('-H', '--host', default='localhost', help='Host')
+    parser.add_argument('-P', '--port', type=int, default=50000, help='Port')
+    parser.add_argument('-s', '--schema', default='db2inst1', help='Schema')
+    parser.add_argument('-m', '--minnr', type=int, default=0, help='Minimum number of indexes')
+    parser.add_argument('-n', '--maxnr', type=int, default=100, help='Maximum number of indexes')
 
-minnr = 0
-maxnr = 100
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "d:u:p:i:h:P:s:m:n:")
-except getopt.GetoptError:
-    sys.exit(-1)
-for o, a in opts:
-    if o == "-d":
-        db = a
-    if o == "-u":
-        user = a
-    if o == "-p":
-        pwd = a
-    if o == "-i":
-        infile = a
-    if o == "-h":
-        host = a
-    if o == "-P":
-        port = a
-    if o == "-s":
-        schema = a
-    if o == "-m":
-        minnr = int(a)
-    if o == "-n":
-        maxnr = int(a)
+    return parser.parse_args()
 
-# check
-if None in [db, user, pwd, infile]:
-    print("Usage: ./index_evaluator.py -d <db> -u <user> -p <pwd> -i <infile> [-h <host> -P <port> -s <schema>]")
-    sys.exit(1)
+def connect_to_db(db, host, port, user, pwd):
+    cfg = (db, host, port, user, pwd)
+    return ibm_db.connect("DATABASE=%s;HOSTNAME=%s;PORT=%s;PROTOCOL=TCPIP;UID=%s;PWD=%s" % cfg, "", "")
 
-cfg = (db, host, port, user, pwd)
-conn = ibm_db.connect("DATABASE=%s;HOSTNAME=%s;PORT=%s;PROTOCOL=TCPIP;UID=%s; PWD=%s" % cfg, "", "")
+def read_query_from_file(infile):
+    query = ""
+    with open(infile, "r") as file:
+        for line in file:
+            query += " " + line
+    return query
 
-# read query from file
-query = ""
-for line in open(infile, "r"):
-    query += " " + line
+def create_procedure(conn):
+    ibm_db.exec_immediate(conn, CREATE_PROC)
+    ibm_db.exec_immediate(conn, "delete from advise_index")
 
-# create procedure
-ibm_db.exec_immediate(conn, CREATE_PROC)
-ibm_db.exec_immediate(conn, "delete from advise_index")
+def create_cost_table(conn):
+    try:
+        ibm_db.exec_immediate(conn, DDL_COST_TABLE)
+    except:
+        pass
+    ibm_db.exec_immediate(conn, CLEAN_COST_TABLE)
 
-# try to create costtbl
-try:
-    ibm_db.exec_immediate(conn, DDL_COST_TABLE)
-except:
-    # assume it exists
-    pass
+def recommend_indexes(conn, query):
+    print("Begin recommending indexes")
+    ibm_db.callproc(conn, 'EXEC_IN_EXPLAIN_MODE', (fix_query(query), "R"))
+    print("End recommending indexes")
 
-ibm_db.exec_immediate(conn, CLEAN_COST_TABLE)
-print("Begin recomending indexes")
-ibm_db.callproc(conn, 'EXEC_IN_EXPLAIN_MODE', (fix_query(query), "R"))
-print("End recomending indexes")
-
-
-indexes = []
-indexes_sql_stmt = ibm_db.exec_immediate(conn, GET_INDEX_NAMES)
-tpl = ibm_db.fetch_tuple(indexes_sql_stmt)
-while tpl:
-    indexes.append(tpl[0])
+def get_suggested_indexes(conn):
+    indexes = []
+    indexes_sql_stmt = ibm_db.exec_immediate(conn, GET_INDEX_NAMES)
     tpl = ibm_db.fetch_tuple(indexes_sql_stmt)
+    while tpl:
+        indexes.append(tpl[0])
+        tpl = ibm_db.fetch_tuple(indexes_sql_stmt)
+    return set(indexes)
 
-indset = set(indexes)
-print("number of suggested indexes %d" % len(indset))
-print(indset)
-ps = list(power_set(indset))
-n = 0
-print("Evaluating %d index combinations" % (len(ps)))
+def evaluate_index_combinations(conn, query, indset, minnr, maxnr):
+    ps = list(power_set(indset))
+    n = 0
+    print("Evaluating %d index combinations" % (len(ps)))
 
-save_cost = ibm_db.prepare(conn, SAVE_QUERY_COST)
-enable_index = ibm_db.prepare(conn, ENABLE_INDEX)
-disable_index = ibm_db.prepare(conn, DISABLE_INDEX)
-p: []
-for p in ps:
-    n += 1
-    sp = set(p)
-    if len(sp) < minnr or len(sp) > maxnr:
-        continue
+    save_cost = ibm_db.prepare(conn, SAVE_QUERY_COST)
+    enable_index = ibm_db.prepare(conn, ENABLE_INDEX)
+    disable_index = ibm_db.prepare(conn, DISABLE_INDEX)
 
-    print("evaluating combination %d" % n)
+    for p in ps:
+        n += 1
+        sp = set(p)
+        if len(sp) < minnr or len(sp) > maxnr:
+            continue
 
-    # enable all in p
-    for i in p:
-        ibm_db.execute(enable_index, (i,))
+        print("Evaluating combination %d" % n)
 
-    # disable all not in p
-    for i in list(indset - sp):
-        ibm_db.execute(disable_index, (i,))
+        for i in p:
+            ibm_db.execute(enable_index, (i,))
 
-    # evaluate cost for current set of indexes
-    ibm_db.callproc(conn, 'EXEC_IN_EXPLAIN_MODE', (fix_query(query), "E"))
+        for i in list(indset - sp):
+            ibm_db.execute(disable_index, (i,))
 
-    # save cost
-    inds = reduce((lambda x, y: x+","+y), p, "")
-    ibm_db.execute(save_cost, (n, inds))
+        ibm_db.callproc(conn, 'EXEC_IN_EXPLAIN_MODE', (fix_query(query), "E"))
 
-# number of chars needed to display all indexes
-max_len = sum(map((lambda x: len(x)), indexes)) + len(indexes)
+        inds = reduce((lambda x, y: x+","+y), p, "")
+        ibm_db.execute(save_cost, (n, inds))
 
-# result, lowest cost for 1,2,...n indexes from set of 2^n index combinations
-a = []
-stmt = ibm_db.prepare(conn, REPORT_COST)
-ibm_db.execute(stmt, (max_len,))
-tpl = ibm_db.fetch_tuple(stmt)
-a.append(tpl)
-while tpl:
+def display_results(conn, indexes):
+    max_len = sum(map((lambda x: len(x)), indexes)) + len(indexes)
+    a = []
+    stmt = ibm_db.prepare(conn, REPORT_COST)
+    ibm_db.execute(stmt, (max_len,))
     tpl = ibm_db.fetch_tuple(stmt)
     a.append(tpl)
+    while tpl:
+        tpl = ibm_db.fetch_tuple(stmt)
+        a.append(tpl)
 
-print()
-h = ['#', 'Index names', 'Cost', 'Improvement']
-print(tabulate(a[:-1], headers=h))
-print()
-
-question = input("Output Index Combination? (N, 2,3,...)\n")
-
-if question == "N" or question == "1":
-    print("Bye")
-    sys.exit(1)
-else:
-    choice = a[int(question) - 1][1]
-    indexes = choice.split(",")
-    stmt = ibm_db.prepare(conn, DISPLAY_INDEXES)
     print()
-    for i in indexes:
-        ibm_db.execute(stmt, (i,))
-        tpl = ibm_db.fetch_tuple(stmt)[0]
-        result = tpl.replace('"', '').replace(' ASC', '')
-        print(result)
-        print()
+    h = ['#', 'Index names', 'Cost', 'Improvement']
+    print(tabulate(a[:-1], headers=h))
+    print()
 
-ibm_db.commit(conn)
+    return a
+
+def output_index_combination(conn, a):
+    question = input("Output Index Combination? (N, 2,3,...)\n")
+
+    if question == "N" or question == "1":
+        print("Bye")
+        sys.exit(1)
+    else:
+        choice = a[int(question) - 1][1]
+        indexes = choice.split(",")
+        stmt = ibm_db.prepare(conn, DISPLAY_INDEXES)
+        print()
+        for i in indexes:
+            ibm_db.execute(stmt, (i,))
+            tpl = ibm_db.fetch_tuple(stmt)[0]
+            result = tpl.replace('"', '').replace(' ASC', '')
+            print(result)
+            print()
+
+def main():
+    args = parse_arguments()
+
+    db = args.db
+    user = args.user
+    pwd = args.pwd
+    infile = args.infile
+    host = args.host
+    port = args.port
+    schema = args.schema
+    minnr = args.minnr
+    maxnr = args.maxnr
+
+    conn = connect_to_db(db, host, port, user, pwd)
+    query = read_query_from_file(infile)
+
+    create_procedure(conn)
+    create_cost_table(conn)
+    recommend_indexes(conn, query)
+
+    indset = get_suggested_indexes(conn)
+    print("Number of suggested indexes %d" % len(indset))
+    print(indset)
+
+    evaluate_index_combinations(conn, query, indset, minnr, maxnr)
+    a = display_results(conn, indset)
+    output_index_combination(conn, a)
+
+    ibm_db.commit(conn)
+
+if __name__ == "__main__":
+    main()
